22a499809b15af5d76045d163bca43f414f2531c
skenned23 <simplecashathome@gmail.com>
2026-05-01 17:30:26 -0400

    add mobile nav

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ import BookingNote from './components/BookingNote'
 import AwardBadge from './components/AwardBadge';
 import Testimonials from './components/Testimonials'
 import Gallery from './components/Gallery'
+import MobileNav from './components/MobileNav'
 
 
 export const metadata = {
@@ -107,6 +108,7 @@ export default function Home() {
 
       <nav>
         <div className="nav-logo">Waxology <span>Studio</span></div>
+<MobileNav />
         <div className="nav-links">
           <a href="#">Home</a>
           <a href="#services">Services</a>
