2da729333b706a02b5cb1a9b8c6f67cb71147871
skenned23 <simplecashathome@gmail.com>
2026-05-01 15:56:08 -0400

    increase badge scale

--- app/components/AwardBadge.js
+++ app/components/AwardBadge.js
@@ -19,7 +19,7 @@ export default function AwardBadge() {
             width: '100%',
             height: '100%',
             objectFit: 'cover',
-          transform: 'scale(1.18)',
+          transform: 'scale(1.35)',
           }}
         />
       </div>
